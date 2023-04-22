b002e30c7c49a140fac2e34715b25f0782556b2b
Qingyang Xie <qingyangxie@qingyangdembp.devices.brown.edu>
2023-04-22 17:18:52 -0400

    [Add] Add a naive implementation of single add function.

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -8,3 +8,14 @@ Java_com_example_calculator_MainActivity_stringFromJNI(
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_calculator_MainActivity_addFromJNI(
+        JNIEnv* env,
+        jobject,
+        jint a,
+        jint b) {
+    int res = a + b;
+    std::string result = std::to_string(res);
+    return env->NewStringUTF(result.c_str());
+}
--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.calculator.databinding.ActivityMainBinding;
@@ -25,6 +30,22 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = binding.sampleText;
+        EditText et1 = binding.edittextInput1;
+        EditText et2 = binding.edittextInput2;
+        Button bt_cal = binding.buttonCalculate;
+
+        // local variable
+
+        bt_cal.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                int input1 = Integer.parseInt(String.valueOf(et1.getText()));
+                int input2 = Integer.parseInt(String.valueOf(et2.getText()));
+                tv.setText(addFromJNI(input1, input2));
+            }
+        });
+
+
+
         tv.setText(stringFromJNI());
     }
 
@@ -33,4 +54,5 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+    public native String addFromJNI(int a, int b);
 }
